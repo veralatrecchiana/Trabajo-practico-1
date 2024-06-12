8216bf95cba9ef0b622ea13985fb88b1081e72c0
sofikuo <skuo@udesa.edu.ar>
2024-06-12 12:27:35 -0300

    valide que el campo del mail no este vacio para el recupero de contrasena

---
+++ js/recover.js
@@ -0,0 +1,26 @@
+let formulario = document.querySelector('#formulario');
+let checkboxRecupero = document.querySelector('#recupero');
+let campoEmail = document.querySelector('#email');
+
+formulario.addEventListener('submit', function(event){
+    event.preventDefault();
+    let todosOK = true;
+
+    document.querySelector('#error-email').innerHTML = '';
+    document.querySelector('#error-checkbox').innerHTML = '';
+
+    if (campoEmail.value === '') {
+        document.querySelector('#error-email').innerHTML = 'Por favor escriba su email';
+        todosOK = false;
+    }
+
+    if (checkboxRecupero.value == '') {
+        document.querySelector('error-checkbox').innerHTML = 'Por favor acepte el campo Quiero recuperar mi contraseña';
+        todosOK = false;
+    }
+    
+    if (todosOK == true) {
+        this.submit();  
+    }
+    
+});
