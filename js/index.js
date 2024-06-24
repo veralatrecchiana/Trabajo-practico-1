f42596e1dd882c8c63b3d882319c85e6f7f3b91b
sofikuo <skuo@udesa.edu.ar>
2024-06-24 10:07:55 -0300

    termine la home section

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-
 let categoria1 = {
       'accesorios': 'jewelery'
   };
@@ -34,4 +33,39 @@ let categoria1 = {
               console.log(error);
           });
   }
-  
+
+let categoria2 = {
+    'womens-clothing': "women's clothing"
+};
+
+for (let categoria in categoria2) {
+    let url = `https://fakestoreapi.com/products/category/${categoria2[categoria]}`;
+
+    fetch(url)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+            let products = data;
+
+            let lista = document.querySelector(`#${categoria}.lista`);
+            let elementosLista = '';
+
+            for (let i = 0; i < products.length; i++) {
+                let product = products[i];
+                elementosLista += `<article>
+                                    <img src="${product.image}" alt="${product.title}">
+                                    <h2>${product.title}</h2>
+                                    <p>Precio: $${product.price}</p>
+                                    <a href="producto.html?id=${product.id}">Ver más</a>
+                                  </article>`;
+            }
+
+            console.log(elementosLista);
+            lista.innerHTML = elementosLista;
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+}
