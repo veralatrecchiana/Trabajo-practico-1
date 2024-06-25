3ffa0fd4cba52a15036e5a65f0895a4443b1d932
veralatre_ <vlatrecchiana@udesa.edu.ar>
2024-06-25 00:00:19 -0300

    cambie html de category

--- js/saludo.js
+++ js/saludo.js
@@ -1,16 +1,10 @@
-let email = localStorage.getItem("email")
-
+let email = localStorage.getItem ('email');
 if (email){
-    let guardarEmail = json.parse(email);
-    let nombre = guardarEmail.email;
+let saludo = document.querySelector('#bienvenida');
+let login = document.querySelector('#login');
+let registro = document.querySelector ('#registro');
+saludoSpan.innerHTML = 'Bienvenido/a: ${email}';
 
-    let saludo = document.querySelector(".saludo");
-    saludo.innerText += `Bienvenido: ${email}`
-    let login = document.querySelector(".login")
-    let registrar = document.querySelector(".registrar")
-    registrar.style.display = "none"
-    register.style.display = "none"
-}else{
-    let saludo = document.querySelector(".saludo");
-    saludo.innerText = "Inicia secion para guardar tus datos";
-}
+login.style.display = 'none';
+registro.style.display = 'none';
+};
